b11ec66d677aaa4eb72faff52caec006d3c0f57c
Klaus Strele <klaus.strele@strele.com>
2025-07-26 12:45:34 +0200

    iphone camera / zurichat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 "use strict";
 
+
+document.getElementById('cameraInput').addEventListener('change', function(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const img = document.getElementById('preview');
+        img.src = URL.createObjectURL(file);
+        img.style.display = 'block';
+      }
+});
+
+
 async function startCamera() {
     const video = document.getElementById('video');
     const constraints = {
